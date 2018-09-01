4c34d47d062e568a735bf7d0c7e2ca0acbcff845
denshade <lieven.vaneeckhaute@gmail.com>
2018-09-01 17:24:37 +0200

    first version.

--- ts/nn.js
+++ ts/nn.js
@@ -1,23 +1,32 @@
-function nn(input, weights, biases) {
+// network = {unitsPerLayer : [], layer0: {weights : [][], biasPerUnit: []}, layer1: {weights : [][], biasPerUnit : []}}
+function nn(input, network) {
+    var unitsPerLayer = network.unitsPerLayer;
     var previousActivation = input;
     var currentActivation = [];
-    for (var layer = 0; layer < weights.length; layer++) {
-        for (var inputElement = 0; inputElement < previousActivation.length; inputElement++) {
-            currentActivation = [];
-            var currentweight = weights[layer];
-            var bias = biases[layer];
-            for (var nextInput = 0; nextInput < currentweight.length; nextInput++) {
-                if (currentActivation[nextInput] == null) {
-                    currentActivation[nextInput] = 0;
+    for (var layer = 0; layer < unitsPerLayer.length - 1; layer++) {
+        currentActivation = [];
+        var currentLayer = network["layer" + layer];
+        var currentbiasPerUnit = currentLayer.biasPerUnit;
+        for (var currentHiddenElement = 0; currentHiddenElement < unitsPerLayer[layer + 1]; currentHiddenElement++) {
+            var currentHiddenElementUnitWeights = currentLayer.weights[currentHiddenElement];
+            if (currentbiasPerUnit.length != unitsPerLayer[layer + 1]) {
+                throw new Error("The length of the bias for layer " + layer + " is different " + currentbiasPerUnit.length + "  from the weight length " + unitsPerLayer[layer + 1]);
+            }
+            for (var nextInput = 0; nextInput < previousActivation.length; nextInput++) {
+                if (currentActivation[currentHiddenElement] == null) {
+                    currentActivation[currentHiddenElement] = 0;
                 }
-                currentActivation[nextInput] += previousActivation[inputElement] * currentweight[inputElement] + bias[inputElement];
+                currentActivation[currentHiddenElement] += previousActivation[nextInput] * currentHiddenElementUnitWeights[nextInput];
             }
-            for (var k = 0; k < currentActivation.length; k++) {
-                currentActivation[k] = Math.tanh(currentActivation[k]);
-            }
-            previousActivation = currentActivation.slice();
         }
+        for (var k = 0; k < currentActivation.length; k++) {
+            currentActivation[k] = activation(currentActivation[k] + currentbiasPerUnit[k]);
+        }
+        previousActivation = currentActivation.slice();
     }
     return currentActivation;
+}
+function activation(t) {
+    return 1 / (1 + Math.pow(Math.E, -t));
 }
 //# sourceMappingURL=nn.js.map
--- ts/nn.ts
+++ ts/nn.ts
@@ -1,27 +1,41 @@
-function nn(input : number[], weights : number[][], biases : number[][]) : number[]
+// network = {unitsPerLayer : [], layer0: {weights : [][], biasPerUnit: []}, layer1: {weights : [][], biasPerUnit : []}}
+
+function nn(input : number[], network : any) : number[]
 {
+    let unitsPerLayer = network.unitsPerLayer;
     let previousActivation : number[] = input;
     let currentActivation : number[] = [];
-    for (let layer = 0; layer < weights.length; layer++)
+    for (let layer = 0; layer < unitsPerLayer.length - 1; layer++)
     {
-        for (let inputElement = 0; inputElement < previousActivation.length; inputElement++)
+        currentActivation=[];
+        let currentLayer = network["layer"+layer];
+        let currentbiasPerUnit = currentLayer.biasPerUnit;
+        for (let currentHiddenElement = 0; currentHiddenElement < unitsPerLayer[layer + 1]; currentHiddenElement++)
         {
-            currentActivation=[];
-            let currentweight = weights[layer];
-            let bias = biases[layer];
-            for (let nextInput = 0; nextInput < currentweight.length; nextInput++)
+            let currentHiddenElementUnitWeights = currentLayer.weights[currentHiddenElement];
+            if (currentbiasPerUnit.length != unitsPerLayer[layer + 1])
             {
-                if (currentActivation[nextInput] == null) {
-                    currentActivation[nextInput] = 0;
+                throw new Error("The length of the bias for layer " + layer + " is different " + currentbiasPerUnit.length +"  from the weight length " + unitsPerLayer[layer + 1]);
+            }
+
+            for (let nextInput = 0; nextInput < previousActivation.length; nextInput++)
+            {
+                if (currentActivation[currentHiddenElement] == null) {
+                    currentActivation[currentHiddenElement] = 0;
                 }
-                currentActivation[nextInput] += previousActivation[inputElement] * currentweight[inputElement] + bias[inputElement];
+                currentActivation[currentHiddenElement] += previousActivation[nextInput] * currentHiddenElementUnitWeights[nextInput];
             }
-            for (let k = 0; k < currentActivation.length; k++)
-            {
-                currentActivation[k] = Math.tanh(currentActivation[k]);
-            }
-            previousActivation = currentActivation.slice();
         }
+        for (let k = 0; k < currentActivation.length; k++)
+        {
+            currentActivation[k] = activation(currentActivation[k] + currentbiasPerUnit[k]);
+        }
+        previousActivation = currentActivation.slice();
     }
     return currentActivation;
+}
+
+function activation(t : number)
+{
+    return 1/(1+Math.pow(Math.E, -t));
 }
